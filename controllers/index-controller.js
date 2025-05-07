bb30cfd73f42fa012d10a7d681b1077eb9af620e
heydarov93 <heyderov.yashar@mail.ru>
2025-05-07 11:10:09 +0400

    feat: customize showHomePage controller to fetch and display all posts

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,8 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
 
 // Display Home page
 const showHomePage = asyncHandler(async (req, res) => {
-  res.render("index");
+  const posts = await db.getPosts();
+  res.render("index", { posts });
 });
 
 module.exports = {
