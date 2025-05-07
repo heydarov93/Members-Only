2edc64d339a474ae6c98675792cb57ecbdbb088c
heydarov93 <heyderov.yashar@mail.ru>
2025-05-07 11:17:35 +0400

    feat: add queries to get posts and to insert a post

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,6 @@
 const pool = require("../config/database");
 
+// Insert new user
 async function createUser(user) {
   const { email, hashPassword, first_name, last_name } = user;
 
@@ -17,6 +18,7 @@ async function createUser(user) {
   return rows[0]?.id;
 }
 
+// Get user by email
 async function getUserByEmail(email) {
   const { rows } = await pool.query("Select * FROM users WHERE email = $1", [
     email,
@@ -25,8 +27,31 @@ async function getUserByEmail(email) {
   return rows[0];
 }
 
+// Get user by id
 async function getUserById(id) {
   const { rows } = await pool.query("Select * FROM users WHERE id = $1", [id]);
+
+  return rows;
+}
+
+// Insert new post
+async function createPost(post) {
+  const { title, content, user_id } = post;
+
+  sql = `INSERT INTO posts (title, content, user_id)
+       VALUES ($1, $2, $3)
+       RETURNING id`;
+
+  const { rows } = await pool.query(sql, [title, content, user_id]);
+
+  return rows[0]?.id;
+}
+
+// Get all posts
+async function getPosts() {
+  sql = `SELECT * FROM posts`;
+
+  const { rows } = await pool.query(sql);
 
   return rows;
 }
@@ -35,4 +60,6 @@ module.exports = {
   createUser,
   getUserByEmail,
   getUserById,
+  createPost,
+  getPosts,
 };
