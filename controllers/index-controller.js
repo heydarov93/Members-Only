0f13c8184b07e7a22d44ef1b355aece9580f0108
heydarov93 <heyderov.yashar@mail.ru>
2025-05-10 23:19:39 +0400

    feat: send formatDate function to the view to display user-friendly dates

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
+const { formatDate } = require("../utils/format-date");
 
 // Display Home page
 const showHomePage = asyncHandler(async (req, res) => {
   const posts = await db.getPosts();
-  res.render("index", { posts });
+
+  res.render("index", { posts, formatDate });
 });
 
 module.exports = {
