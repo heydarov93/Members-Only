5022852190042e7e4d3f9526276fc96e016c67f9
heydarov93 <heyderov.yashar@mail.ru>
2025-05-11 05:41:22 +0400

    feat: add and update query functions - Added grantRole: grants new role to user - Added updatePost: updates post by id - Added getPostById: retrieves a post by id - Added deletePost: deletes post by id - Updated getPosts: join posts with users table

--- db/queries.js
+++ db/queries.js
@@ -34,6 +34,19 @@ async function getUserById(id) {
   return rows;
 }
 
+// Assign role to user
+async function grantRole(userId, role) {
+  const sql =
+    role === "admin" ? "is_admin = true, is_member = true" : "is_member = true";
+
+  const { rows } = await pool.query(
+    `UPDATE users SET ${sql} WHERE id = $1 RETURNING id`,
+    [userId]
+  );
+
+  return rows[0]?.id;
+}
+
 // Insert new post
 async function createPost(post) {
   const { title, content, user_id } = post;
@@ -47,19 +60,56 @@ async function createPost(post) {
   return rows[0]?.id;
 }
 
+// Update a post
+async function updatePost(post) {
+  const { id, title, content } = post;
+
+  sql = `UPDATE posts SET title = $1, content = $2
+       WHERE id = $3
+       RETURNING id`;
+
+  const { rows } = await pool.query(sql, [title, content, id]);
+
+  return rows[0]?.id;
+}
+
 // Get all posts
 async function getPosts() {
-  sql = `SELECT * FROM posts`;
+  sql = `SELECT posts.*, users.first_name, users.last_name FROM posts JOIN users ON users.id = posts.user_id ORDER BY posts.created_at DESC`;
 
   const { rows } = await pool.query(sql);
 
   return rows;
 }
 
+// Get post by id
+async function getPostById(id) {
+  const { rows } = await pool.query(
+    "Select posts.*, users.first_name, users.last_name  FROM posts JOIN users ON users.id = posts.user_id WHERE posts.id = $1",
+    [id]
+  );
+
+  return rows[0];
+}
+
+// Delete post by id
+async function deletePost(id) {
+  const { rows } = await pool.query(
+    "DELETE FROM posts WHERE id = $1 RETURNING id",
+    [id]
+  );
+
+  return rows[0]?.id;
+}
+
 module.exports = {
   createUser,
   getUserByEmail,
   getUserById,
+  grantRole,
   createPost,
+  updatePost,
   getPosts,
+  getPostById,
+  deletePost,
 };
