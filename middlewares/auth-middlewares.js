723004bb87871a9d4f88594f5a8991e525cb0c26
heydarov93 <heyderov.yashar@mail.ru>
2025-05-11 05:45:25 +0400

    feat: add authorization middlewares and modify authentication one - Updated isAuth: if user unauthenticated redirect it instead of throwing an error - Added isNotAuth: checks if user is not authenticated - Added isAdmin: checks if user is an admin - Added isMember: checks if user is a memeber of the club

--- middlewares/auth-middlewares.js
+++ middlewares/auth-middlewares.js
@@ -1,15 +1,45 @@
+const CustomForbiddenError = require("../error/CustomForbiddenError");
 const CustomUnauthorizedError = require("../error/CustomUnauthorizedError");
 
+// Check if user logged in
 const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
-    throw new CustomUnauthorizedError(
-      "You are not authorized to view this resource"
-    );
+    res.redirect("/auth/login");
   }
+};
+
+// Check if the user is NOT authenticated
+const isNotAuth = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
+
+// Check if user is admin
+const isAdmin = (req, res, next) => {
+  if (req.user.is_admin) return next();
+
+  throw new CustomForbiddenError(
+    "You don't have a permission for this action, only admins"
+  );
+};
+
+// Check if user is the member of the private clubhouse
+const isMember = (req, res, next) => {
+  if (req.user.is_member) return next();
+
+  throw new CustomForbiddenError(
+    "You don't have a permission for this action, only members"
+  );
 };
 
 module.exports = {
   isAuth,
+  isNotAuth,
+  isAdmin,
+  isMember,
 };
